51fe94a760fc6980ac08c824845754375fb7db08
Shivani Singh <cwani888@gmail.com>
2021-06-09 00:33:58 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-web-server',
+    version='1.0',
+    packages=['demo_server'],
+    url='',
+    license='',
+    author='Shivani',
+    author_email='cwani888@gmail.com',
+    description='demo web server'
+)
